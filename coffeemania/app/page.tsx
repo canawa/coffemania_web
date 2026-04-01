32d6285bbb63c5c578dc448295a8b518c6378ed7
den <faceitlolaw@gmail.com>
2026-04-01 14:52:14 +0300

    integration

--- coffeemania/app/page.tsx
+++ coffeemania/app/page.tsx
@@ -104,7 +104,7 @@ export default function Home() {
 
             <div className="relative">
               <div className="absolute -top-20 -right-20 w-96 h-96 bg-tertiary-fixed opacity-20 blur-3xl rounded-full" />
-               <div className="relative w-full max-w-[520px] mx-auto aspect-square hover:rotate-0 transition-transform duration-700">
+               <div className="relative w-full max-w-[520px] mx-auto aspect-square rotate-[-3deg] hover:rotate-[5deg] transition-transform duration-500 ease-out">
                  <Image
                   className="w-full h-full object-contain"
                   alt="Минималистичная кофейная чашка и смартфон"
